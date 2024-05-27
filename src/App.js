70a27177801e842c74bdf184ec7a5a1d1a389ccb
RP <rahulpal80524@gmail.com>
2024-05-27 18:24:56 +0530

    tailwind css and event handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import weblog from "./Images/download.jpeg"
 function App() {
+  let displayData=()=>{
+    alert("Welcome")
+  }
+
+  let message=(a,b)=>{
+   console.log(a+b)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='bg-[red] p-[]10px' onClick={()=>message(12,34)}>parameter use</button> 
+      <button className='bg-[red] p-[]10px' onClick={displayData}>Save</button>
+      {/* <button className='bg-[red] p-[]10px' onClick={displayData()}>Save</button>  if displayData() auto function call if page is load*/}
+
+      <Header/>
+      <img src= {weblog}/>
+      <h1 className='text-[40px] text-red-200'> welcome to web application 1</h1>
     </div>
   );
 }
